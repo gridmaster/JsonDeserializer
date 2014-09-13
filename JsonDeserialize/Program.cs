79ef2cb03770fffa82263a0ee1ad48c852013e91
gridmaster <antlerhorse@gmail.com>
2014-09-13 13:10:51 -0400

    cleaned up code

--- JsonDeserialize/Program.cs
+++ JsonDeserialize/Program.cs
@@ -12,9 +12,8 @@ namespace JsonDeserialize
 {
     class Program
     {
-        //private static string JsonInput =
-        //"{"sector":[{"name":"Basic Materials","industry":[{"id":"112","name":"Agricultural Chemicals"},{"id":"132","name":"Aluminum"}]},{"name":"Conglomerates","industry":{"id":"210","name":"Conglomerates"}},{"name":"Consumer Goods","industry":[{"id":"310","name":"Appliances"},{"id":"344","name":"Dairy Products"},{"id":"314","name":"Electronic Equipment"},{"id":"331","name":"Trucks &amp; Other Vehicles"}]},{"name":"Financial","industry":[{"id":"431","name":"Accident &amp; Health Insurance"},{"id":"422","name":"Asset Management"},{"id":"433","name":"Surety &amp; Title Insurance"}]},{"name":"Utilities","industry":[{"id":"913","name":"Diversified Utilities"},{"id":"911","name":"Electric Utilities"},{"id":"910","name":"Foreign Utilities"},{"id":"912","name":"Gas Utilities"},{"id":"914","name":"Water Utilities"}]}]}";"
-        public static string url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.sectors&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+        public static string urlSectorIndustry = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.sectors&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+        public static string urlIndustryCompany = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.industry%20where%20id%20in%20(select%20industry.id%20from%20yahoo.finance.sectors)&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
        
         public static string GetResponse(string sUri)
         {
@@ -47,7 +46,13 @@ namespace JsonDeserialize
         public class BaseSectors
         {
             [JsonProperty(PropertyName = "sector")]
-            public List<Sectors> Companies { get; set; }
+            public List<Sectors> Sectors { get; set; }
+        }
+
+        public class BaseIndustries
+        {
+            [JsonProperty(PropertyName = "industry")]
+            public List<Industry> Industries { get; set; }
         }
 
         public class Sectors
@@ -63,12 +68,71 @@ namespace JsonDeserialize
         {
             [JsonProperty(PropertyName = "id")]
             public string Id { get; set; }
- 
+
             [JsonProperty(PropertyName = "name")]
             public string Name { get; set; }
+
+            [JsonProperty(PropertyName = "company")]
+            public List<Company> Companies { get; set; } 
         }
 
-        static void Main(string[] args)
+        public class Company
+        {
+            [JsonProperty(PropertyName = "name")]
+            public string Name { get; set; }
+
+            [JsonProperty(PropertyName = "symbol")]
+            public string Symbol { get; set; }
+        }
+
+        static BaseSectors GetSectorsIndustires()
+        {
+            string json = GetJson(urlSectorIndustry);
+
+            json = CleanJson(json);
+
+            while (json.IndexOf("\"industry\":{\"id\"", System.StringComparison.Ordinal) > -1)
+            {
+                var regex = new Regex(Regex.Escape("\"industry\":{\"id\""));
+                json = regex.Replace(json, "\"industry\":[{\"id\"", 1);
+
+                regex = new Regex(Regex.Escape("}},{"));
+                json = regex.Replace(json, "}]},{", 1);
+            }
+
+            return JsonConvert.DeserializeObject<BaseSectors>(json);
+        }
+
+        static BaseIndustries GetIndustiresCompanies()
+        {
+            string json = GetJson(urlIndustryCompany);
+
+            json = CleanJson(json);
+
+            string problem = "{\"id\":\"133\",\"name\":\"\"},";
+            int ndx = json.IndexOf(problem, System.StringComparison.Ordinal);
+
+            json = json.Replace(problem, "");
+
+            problem = "\"company\":{";
+
+            while (json.IndexOf(problem, System.StringComparison.Ordinal) > -1)
+            {
+                ndx = json.IndexOf(problem, System.StringComparison.Ordinal);
+
+                var regex = new Regex(Regex.Escape(problem));
+                json = regex.Replace(json, "\"company\":[{", 1);
+
+                ndx += json.Substring(ndx).IndexOf("}", System.StringComparison.Ordinal);
+
+                json = json.Substring(0, ndx + 1) + "]" + json.Substring(ndx + 1); 
+            }
+            var crap = json.Substring(1098068);
+
+            return JsonConvert.DeserializeObject<BaseIndustries>(json);
+        }
+
+        private static string GetJson(string url)
         {
             int i = 0;
             string json = string.Empty;
@@ -80,46 +144,40 @@ namespace JsonDeserialize
             } while (json == "WebWorks.GetResponse ERROR: Unable to connect to the remote server");
 
             Console.WriteLine("i = " + i);
+
             XmlDocument doc = new XmlDocument();
             doc.LoadXml(json);
-            json = Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc);
+            return Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc);
+        }
 
+        private static string CleanJson(string json)
+        {
+            string newjson = json.Substring(json.IndexOf("\"results\":", System.StringComparison.Ordinal) + "\"results\":".Length);
+            newjson = newjson.Substring(0, newjson.IndexOf("}]}]}", System.StringComparison.Ordinal) + "}]}]}".Length);
+            newjson = newjson.Replace("\r", "").Replace("\t", "").Replace("\n", "");
+            return newjson.Replace("@", "");
+        }
 
-            json = json.Replace("\r", "").Replace("\t", "").Replace("\n", "");
-            json = json.Replace("@", "");
-            while (json.IndexOf("\"industry\":{\"id\"", System.StringComparison.Ordinal) > -1)
+        static void Main(string[] args)
+        {
+            BaseSectors bs = GetSectorsIndustires();
+
+            BaseIndustries bi = GetIndustiresCompanies();
+
+            foreach (Sectors s in bs.Sectors)
             {
-                var regex = new Regex(Regex.Escape("\"industry\":{\"id\""));
-                json = regex.Replace(json, "\"industry\":[{\"id\"", 1);
-
-                regex = new Regex(Regex.Escape("}},{"));
-                json = regex.Replace(json, "}]},{", 1);
+                foreach (Industry i in s.Industries)
+                {
+                    var wtf = bi.Industries.Find(ind => ind.Id == i.Id);
+                    if (wtf == null) continue;
+                    
+                    i.Companies = wtf.Companies;
+                }
             }
 
-            var result = JsonConvert.DeserializeObject<BaseSectors>(json);
+            string json = JsonConvert.SerializeObject(bs);
 
             Console.ReadKey();
-
-            using (StreamReader sr = new StreamReader(@"C:\Projects\JsonDeserialize\JsonDeserialize\Sectors.txt"))
-            {
-                json = sr.ReadToEnd();
-
-                Console.WriteLine(json);
-                json = json.Replace("\r", "").Replace("\t", "").Replace("\n", "");
-                json = json.Replace("@", "");
-                while (json.IndexOf("\"industry\":{\"id\"", System.StringComparison.Ordinal) > -1)
-                {
-                    var regex = new Regex(Regex.Escape("\"industry\":{\"id\""));
-                    json = regex.Replace(json, "\"industry\":[{\"id\"", 1);
-
-                    regex = new Regex(Regex.Escape("}},{"));
-                    json = regex.Replace(json, "}]},{", 1);
-                }
-
-                result = JsonConvert.DeserializeObject<BaseSectors>(json);
-
-                Console.ReadKey();
-            }
         }
     }
 }
